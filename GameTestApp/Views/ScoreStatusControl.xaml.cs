50eec8f8318b530a27dc65ea372e39285b6cff54
rmccavery <richardmccavery@hotmail.com>
2021-02-11 10:23:18 +0000

    Initial revision

---
+++ GameTestApp/Views/ScoreStatusControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace GameTestApp.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ScoreStatusControl.xaml
+    /// </summary>
+    public partial class ScoreStatusControl : UserControl
+    {
+        public ScoreStatusControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
